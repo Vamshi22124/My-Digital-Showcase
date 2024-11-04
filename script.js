fc552cfc4533bfc5b73f7398d809cb6b57cc9119
Vamshi22124 <vamshi0467@gmail.com>
2024-11-04 10:50:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Basic form validation or interactive features can go here
-document.querySelector('form').addEventListener('submit', function(event) {
-    alert('Your message has been sent!');
-    event.preventDefault();  // For demonstration only, you'd handle the actual form submission here
-});
